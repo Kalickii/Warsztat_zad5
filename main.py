6273b1ce35a1908cedf2865b8020f836e5c4904d
Michał <michal.kalicki@outlook.com>
2024-04-28 13:29:02 +0200

    Fixed valid number checking

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import re
+from random import randint
 
 def dice(code):
     dice_types = ["D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100"]
     try:
-        found_dice = re.findall(r"D\d+", code)
+        found_dice = re.findall(r"D\d+", code)[0]
 
-        valid_dice = [dice for dice in found_dice if dice in dice_types]
+        if found_dice in dice_types:
+            valid_dice = int(found_dice[1::])
 
-        throws = [re.findall(r"\d+", code)[0]]
+        throws = int(re.findall(r"\d+", code)[0])
 
         modifier = int(re.findall(r"\d+", code)[2])
         if "-" in code:
